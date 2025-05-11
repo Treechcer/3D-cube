09ca6611875e1e702e8f8296da17d0d93fe35827
Treecher <martin.vrobel308@gmail.com>
2025-05-11 21:35:32 +0200

    made the scalable cube work

--- main.lua
+++ main.lua
@@ -11,41 +11,35 @@ function love.update(dt)
     io.flush();
     -- this prints out "test" and updates it in VSC because for some reason it doesn't normaly
     ]]
-
-    if love.keyboard.isDown("space") then
-        print("space");
-        io.flush();
-    end
 end
 
 function love.draw()
     -- love.graphics.print("test", 25, 25); --epic
-    x = 100;
 
-    drawScaleableCube(100,1/2)
+    drawCube(100);
+    drawScaleableCube(300,300,100);
 
 end
 
-function drawScaleableCube(x, scale)
-    y = x;
+function drawScaleableCube(x, y, size)
+    -- first square
+    love.graphics.line(x,y,x+size,y);
+    love.graphics.line(x,y,x,y+size);
+    love.graphics.line(x+size,y,x+size,y+size);
+    love.graphics.line(x,y+size,x+size,y+size);
 
-    love.graphics.line(x,y,2 * (x/scale),y);
-    love.graphics.line(x,y,x,2 * (y/scale));
+    -- second square
 
-    love.graphics.line(2 * (x/scale),y,2 * (x/scale),2 * (y/scale));
-    love.graphics.line(x,2 * (y/scale),2 * (x/scale),2 * (y/scale));
+    love.graphics.line(x+(size/2),y-(size/2),x+(size/2),y+(size/2));
+    love.graphics.line(x+(size/2)+size,y-(size/2),x+(size/2)+size,y+(size/2));
+    love.graphics.line(x+size+size/2,y-size/2,x+size/2,y-size/2);
+    love.graphics.line(x+size+size/2,y+size/2,x+size/2,y+size/2);
 
-    love.graphics.line(2 * (x/scale),2 * (y/scale),2 * (x/scale) + (x/4),y + (3 * ((y/scale)/4)));
-    love.graphics.line(x,2 * (y/scale),(x/scale) - (x/4),y + (3 * ((y/scale)/4)));
-
-    love.graphics.line(x,y,(x/scale) + (x/4), y - ((y/scale)/4));
-    love.graphics.line(2 * (x/scale),y,2 * x + ((x/scale)/4),(y/scale) + (y/4));
-
-    --love.graphics.line((x/scale) + (x/4),(y/scale) - (y/4),2 * x + (x/scale/4),y - (y/scale/4));
-    --love.graphics.line(2 * (x/scale) + (x/4),(y/scale) - (y/4),2 * x + (x/scale/4),y + (3 * (y/scale/4)));
-
-    --love.graphics.line((x/scale) + (x/4),y + (3 * (y/scale/4)),x + (x/scale/4), y - (y/scale/4));
-    --love.graphics.line((x/scale) + (x/4),2 * y - (y/scale/4), 2 * x + (x/scale/4),2 * y - (y/scale/4));
+    -- 3DS effect
+    love.graphics.line(x+(size/2),y-(size/2),x,y);
+    love.graphics.line(x+size+(size/2),y-(size/2),x+size,y);
+    love.graphics.line(x+(size/2),y+(size/2),x,y+size);
+    love.graphics.line(x+(size/2)+size,y+(size/2),x+size,y+size);
 end
 
 function drawCube(x)
